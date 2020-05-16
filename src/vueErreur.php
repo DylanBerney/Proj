12a0b1aa323aed23063431674ed6208b2927bda0
Luca <luca.bassi@cpnv.ch>
2020-05-16 16:06:57 +0200

    suite

--- src/vueErreur.php
+++ src/vueErreur.php
@@ -12,7 +12,7 @@ $log = "User: " . $_SERVER['REMOTE_ADDR'] . ' - ' . date("F j, Y, g:i a") . PHP_
     "User: " . $php_errormsg . PHP_EOL .
     "-------------------------" . PHP_EOL;
 //Save string to log, use FILE_APPEND to append.
-file_put_contents('./log_' . date("j.n.Y") . '.log', $log, FILE_APPEND);
+file_put_contents('./log_exceptions.log', $log, FILE_APPEND);
 
 ?>
     <br><br><br><br>
