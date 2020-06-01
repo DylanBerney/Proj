b6716c99f52410199eebc6fe0e273ebd258eab9c
LucaBassi <luca.bassi@hotmail.fr>
2020-06-01 14:31:21 +0200

    try view photo panier

--- src/view/panier.php
+++ src/view/panier.php
@@ -19,80 +19,72 @@ ob_start();
                 <div class="site-section mt-5">
                     <div class="container">
                         <div class="row">
+
+
+                            <?php
+                            $myIndex = 0;
+                            if (isset($_SESSION['wine'])) {
+                            $totalPrice = 0;
+                            foreach ($_SESSION['wine'] as $wesh) {
+
+
+                            $qtySel = $_SESSION['wine'][$myIndex]['qty'];
+                            $totalQty = $_SESSION['wine'][$myIndex]['totalQty'];
+                            $marque = $_SESSION['wine'][$myIndex]['marque'];
+                            $modele = $_SESSION['wine'][$myIndex]['modele'];
+                            $price = $_SESSION['wine'][$myIndex]['price'];
+                            $photo = $_SESSION['wine'][$myIndex]['photo'];
+                            $id = $_SESSION['wine'][$myIndex]['id'];
+                            $totalPrice = $totalPrice + $_SESSION['wine'][$myIndex]['price'];
+
+                            if (isset($_SESSION['wine'][$myIndex])) { ?>
+
                             <table class="table">
+                                <tr>
+                                    <td>
+                                        <h3>Marque </h3>
+                                        <h4><?= $marque ?></h4>
+                                    </td>
+                                    <td>
+                                        <h3>Modèle </h3>
+                                        <h4><?= $modele ?></h4>
+                                    </td>
 
+                                    <td colspan="5">
+                                        <img style='alignment=right;max-width: 150px  ' src=<?= $photo ?>/>
+                                    </td>
+                                    <td>
+                                        <h3>Prix</h3>
+                                        <h4><?= $price ?></h4>
+                                    </td>
 
-                                <?php
-                                $myIndex = 0;
-                                if (isset($_SESSION['wine'])) {
-                                $totalPrice = 0;
-                                foreach ($_SESSION['wine'] as $wesh) {
-
-
-                                $qtySel = $_SESSION['wine'][$myIndex]['qty'];
-                                $totalQty = $_SESSION['wine'][$myIndex]['totalQty'];
-                                $marque = $_SESSION['wine'][$myIndex]['marque'];
-                                $modele = $_SESSION['wine'][$myIndex]['modele'];
-                                $price = $_SESSION['wine'][$myIndex]['price'];
-                                $photo = $_SESSION['wine'][$myIndex]['photo'];
-                                $id = $_SESSION['wine'][$myIndex]['id'];
-                                $totalPrice = $totalPrice + $_SESSION['wine'][$myIndex]['price'];
-
-                                if (isset($_SESSION['wine'][$myIndex])) {
-                                ?>
-
-                                <table class="table">
-                                    <tr aria-colspan="18"
-                                    ">
-                                    <tr class="">
-                                    <tr>
-                                        <td>
-                                            <h3>Marque </h3>
-                                            <h4><?= $marque ?></h4>
-                                        </td>
-                                        <td>
-                                            <h3>Modèle </h3>
-                                            <h4><?= $modele ?></h4>
-                                        </td>
-
-                                        <td>
-                                            <!--  <h3>quantité </h3>-->
-                                        <td colspan="5">
-                                            <img style='alignment=right;max-width: 150px  ' src=<?= $photo ?>/>
-                                        </td>
-                                        <td>
-                                            <h3>Prix</h3>
-                                            <h4><?= $price ?></h4>
-                                        </td>
-
-                                    </tr>
-                                </table>
-                                <td>
-                                    <!--  <h3>quantité </h3>-->
-                                <td colspan="">
-                                    <label>
-                                        <input class="input-small" type="number" min="1" value="<?= $qtySel ?>">
-                                    </label>
-                                    <button class="btn btn-medium">Changer quantité</button>
-                                    <br>
-                                    <br>
-                                    <br>
-
-                                    <form method="post" action=" index.php?action=userDelwine">
-                                        <input type="hidden" name="id" value="<?= $id ?>">
-                                        <button class="btn btn-medium" type="submit" style="color: red">Supprimer
-                                        </button>
-                                    </form>
-                                    <br>
-                                </td>
+                                </tr>
                             </table>
+                        </div>
+                        <div>
+                            <table>
+                                <tr>
+                                    <td>
+                                        <label>
+                                            <input class="input-small" type="number" min="1" value="<?= $qtySel ?>">
+                                        </label>
+                                        <button class="btn btn-medium">Changer quantité</button>
+                                        <br>
+                                        <br>
+                                        <br>
 
+                                        <form method="post" action=" index.php?action=userDelwine">
+                                            <input type="hidden" name="id" value="<?= $id ?>">
+                                            <button class="btn btn-medium" type="submit" style="color: red">Supprimer
+                                            </button>
+                                        </form>
+                                        <br>
+                                    </td>
+                            </table>
                             <?php }
                             $myIndex++;
                             }
                             } ?>
-
-
                         </div>
                     </div>
                 </div>
@@ -115,12 +107,13 @@ ob_start();
                         <button class="btn" type="submit" style="color: red">Vider le Panier</button>
                         <!--         <input type="hidden" name="id" value="<? /*= $id */ ?>">-->
                         <label class="input-medium">
+                        </label>
                     </form>
                 </div>
                 <div class="btn-group">
                     <form method="post" action="index.php?action=command">
                         <label class="input-medium">
-                            <button class="btn" type="submit">Commander</button>
+                            <button class="btn" type="submit">Commander</button></label>
                     </form>
                 </div>
             <?php endif; ?>
