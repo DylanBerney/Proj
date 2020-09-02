87351a71d9ac9ffd56c55b630c62491c68d1f3ba
[Jan] <[Jan.blatter@cpnv.ch]>
2020-09-02 09:07:45 +0200

    Update login

--- src/view/login.php
+++ src/view/login.php
@@ -4,17 +4,16 @@
 ob_start();
 ?>
 <br>
-    <form  align="center" class="form" method="POST" action="index.php?action=loginIsCorrect">
+    <form  align="center" class="form" method="POST" action="index.php?action=login">
         <br>
         <h1> Login </h1>
         <b>Email :</b><br>
         <input  type="text" name="inputUserEmailAddress" value="" placeholder="Adresse Email" required>
         <br>
         <b>Mot de Passe:</b><br>
-        <input type="password" name="inputUserPsw'" value="" placeholder="Mot de passe" required>
+        <input type="password" name="inputUserPsw" value="" placeholder="Mot de passe" required>
         <br>
-        <br>
-        <input type="submit" value="Login"> <input type="button" value="Reset"> Forgot <a href="">password</a> ?
+        <input type="submit" value="login"> <input type="button" value="Reset"> Forgot <a href="">password</a> ?
     </form>
 
 
