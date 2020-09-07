d3ed58563f658b943439ebf1776a5284a5eda733
luca <luca.bassi@cpnv.ch>
2020-09-07 13:18:11 +0200

    solution reception

--- src_OOP/tests.php
+++ src_OOP/tests.php
@@ -1,45 +1,33 @@
-
-
-<!-- index.php -->
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    </head>
-    <body>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body>
     <form id="myForm">
         <input type="text" name="fname" id="fname"/>
         <input type="submit" name="click" value="button" />
     </form>
     <script>
-    $(document).ready(function(){
-
-         $(function(){
-            $("#myForm").submit(function(event){
-                event.preventDefault();
-                $.ajax({
-                    method: 'POST',
-                    url: 'submit.php',
-                    dataType: "json",
-                    contentType: "application/json",
-                    data : $('#myForm').serialize(),
-                    success: function(data){
-                        alert(data);
-                    },
-                    error: function(xhr, desc, err){
-                        console.log(err);
-                    }
-                });
+        $('#myForm').click(function () {
+            $.ajax({
+                 url : 'view/ajaxJsonProcess.php',  
+                type: 'POST',
+           
+            data: {
+                     
+                    email: 'email@example.com',
+                    message: 'hello world!'
+                },
+                success: function (msg) {
+                    alert('Email Sent');
+                }
             });
         });
-    });
     </script>
-    </body>
-    </html>
+</body>
+</html>
 <!-- submit.php -->
 <?php
-$value ="call";
-header('Content-Type: application/json');
+$value = "call";
 echo json_encode($value);
 ?>
 
