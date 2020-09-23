48dc35416a1e955f26d43499c83907881b218042
Berney <dylan.berney@cpnv.ch>
2020-09-23 10:35:06 +0200

    Mise à niveau de la description de l'article

--- src/view/wine.php
+++ src/view/wine.php
@@ -35,9 +35,7 @@ ob_start();
                                             <div>
                                                 <h3 class="heading mb-1"><a href="#"><?= $wines["brand"] ?></a></h3>
                                                 <h5 class="heading mb-2"><a href="#"><?= $wines["model"] ?></a></h5>
-                                                <h6 class="heading mb-6"><a href="#">Poucentage
-                                                        d'alcool: <?= $wines["alchoholpercent"] ?> %</a></h6>
-                                                <span class="price">$<?= $wines["price"] ?></span>
+
                                             </div>
 
 
@@ -47,70 +45,7 @@ ob_start();
                                                 <h3 class="heading-2"><a href="#"></a></h3>
                                                 <span class="price d-block"><del>$<?= $wines["oldPrice"] ?></del> $<?= $wines["price"] ?></span>
 
-                                                <div class="rating">
-                                                    <?php
-                                                    $nbStar = $wines['rating'];
-                                                    if ($nbStar == 0) { {
-                                                            ?>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <?php
-                                                        }
-                                                    } elseif ($nbStar == 1) { {
-                                                            ?>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <?php
-                                                        }
-                                                    } elseif ($nbStar == 2) { {
-                                                            ?>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <?php
-                                                        }
-                                                    } elseif ($nbStar == 3) { {
-                                                            ?>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <?php
-                                                        }
-                                                    } elseif ($nbStar == 4) { {
-                                                            ?>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star-o"></span>
-                                                            <?php
-                                                        }
-                                                    } elseif ($nbStar == 5) { {
-                                                            ?>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <span class="icon-star"></span>
-                                                            <?php
-                                                        }
-                                                    } else { {
-                                                            $noValue = 'Notation Inconnue';
-                                                        }
-                                                    }
-                                                    ?>
 
-                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -136,6 +71,72 @@ ob_start();
                                                         <div class="collapse" id="ship_different_address">
                                                             <div>
                                                                 <p><?= $wines["description"] ?></p>
+                                                                <p>Poucentage d'alcool: <?= $wines["alchoholpercent"] ?></p>
+                                                                <span class="price">$<?= $wines["price"] ?></span>
+                                                            </div>
+                                                            <div class="rating">
+                                                                <?php
+                                                                $nbStar = $wines['rating'];
+                                                                if ($nbStar == 0) { {
+                                                                    ?>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <?php
+                                                                }
+                                                                } elseif ($nbStar == 1) { {
+                                                                    ?>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <?php
+                                                                }
+                                                                } elseif ($nbStar == 2) { {
+                                                                    ?>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <?php
+                                                                }
+                                                                } elseif ($nbStar == 3) { {
+                                                                    ?>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <?php
+                                                                }
+                                                                } elseif ($nbStar == 4) { {
+                                                                    ?>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star-o"></span>
+                                                                    <?php
+                                                                }
+                                                                } elseif ($nbStar == 5) { {
+                                                                    ?>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <span class="icon-star"></span>
+                                                                    <?php
+                                                                }
+                                                                } else { {
+                                                                    $noValue = 'Notation Inconnue';
+                                                                }
+                                                                }
+                                                                ?>
+
                                                             </div>
                                                         </div>
                                                     </div>
