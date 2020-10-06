3e3303e94fea60456190e6c0a28d52db122d20dc
[Jan] <[Jan.blatter@cpnv.ch]>
2020-10-06 14:23:06 +0200

    Upgrade de Contact us

--- src/view/contact.php
+++ src/view/contact.php
@@ -6,8 +6,8 @@ $title = "Rent A Snow - Home";
 
 <?php
 if(isset($_POST['submit'])){
-    $to = "Dylan.Berney@cpnv.ch"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+    $to = $_POST['email']; // this is your Email address
+    $from = 'Wines@hotmail.com'; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $subject = "Form submission";
@@ -18,8 +18,7 @@ if(isset($_POST['submit'])){
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Email envoyé " . $first_name . ", Nous vous contacterons.";
 
 }
 ?>
